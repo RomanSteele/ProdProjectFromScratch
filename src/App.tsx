05f5d5a3d309db4cba51bae154b9905be74ec6a5
«Roman <romanzelenenko@yandex.ru>
2023-12-14 18:42:10 -0500

    feat: setting up css modules

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.scss';
+import Counter from './components/counter';
+
+const App = () => {
+    return (
+        <div className='app'>
+            Ciao!
+            <Counter />
+        </div>
+    );
+};
+
+export default App;
--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,19 +1,17 @@
-import './counter.scss';
+import classes from './counter.module.scss';
 import { useState } from 'react';
 
  export const Counter = () => {
-const [count,setCount] = useState(0);
+const [count, setCount] = useState(0);
 
 const increment = () => {
     setCount(count+1)
 }
 
     return (
-        <div>
+        <div className={classes.btn}>
             <h1>{count}</h1>
-        <button onClick={increment}>
-           increment 
-        </button>
+                <button onClick={increment}>increment </button>
         </div>
     );
 };
