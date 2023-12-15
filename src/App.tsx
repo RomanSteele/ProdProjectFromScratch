93efb0f09ea039592f3e2a59232d6bb215c21666
«Roman <romanzelenenko@yandex.ru>
2023-12-15 10:06:21 -0500

    feat: code splitting, lazy loading, suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './index.scss';
-import Counter from './components/counter';
+import { Link, Route, Routes } from 'react-router-dom';
+import { AboutPageAsync } from './pages/about-page/about-page.async';
+import { MainPageAsync } from './pages/main-page/main-page.async';
+import { Suspense } from 'react';
 
 const App = () => {
     return (
         <div className='app'>
-            Ciao!
-            <Counter />
+            <Link to={'/'}>MAIN </Link>
+            <Link to={'/about'}> ABOUT</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element = {<AboutPageAsync/>}/>
+                    <Route path={'/'} element = {<MainPageAsync />}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
