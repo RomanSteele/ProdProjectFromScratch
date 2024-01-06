5b4c25249f1a9ddeee758848734dc3f56d7bcdcc
«Roman <romanzelenenko@yandex.ru>
2024-01-06 09:40:18 -0500

    fix: delete test counter

--- src/pages/main-page/ui/main-page.tsx
+++ src/pages/main-page/ui/main-page.tsx
@@ -1,12 +1,10 @@
 import { useTranslation } from 'react-i18next';
-import { Counter } from '../../../entities/counter';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
     return (
         <div>
             {t('Main page')}
-            <Counter />
         </div>
     );
 };
